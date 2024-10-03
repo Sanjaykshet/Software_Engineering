83b85100b26fbf5b440b9d879b602085f639ecc1
sanjaykshet <sanjaykshet@gmail.com>
2024-10-03 19:20:26 +0530

    add call by value code

--- callbyvalue.c
+++ callbyvalue.c
@@ -3,17 +3,12 @@ void calculate(int,int);
 int main(){
     int a,b;
     printf("enter a and b\n");
-    scanf("%d,%d",&a,&b);
+    scanf("%d%d",&a,&b);
     calculate(a,b);
 }
 
 void calculate(int x,int y){
-    printf("\n1-addition\n2-substraction\n3-multiplication\n4-division");
-    int num,r;
-    scanf("%d",&num);
-    while(1){
-        if(num==1){
-            printf("sum is ");
-        }
-    }
+    x = x-1;
+    int sum = x+y;
+    printf("the sum is %d",sum);
 }
